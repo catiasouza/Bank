e7566c487cd9a206f2eec782bf0b67e6e13454f1
Catia <catiamsouza80@gmail.com>
2021-03-29 09:12:16 -0300

    implementando post

--- DesafioSantanderIOS/Controller/LoginVC.swift
+++ DesafioSantanderIOS/Controller/LoginVC.swift
@@ -12,6 +12,7 @@ class LoginVC: UIViewController {
     @IBOutlet weak var user: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var loginButton: UIButton!
+    let networkingService = NetworkingService()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +21,20 @@ class LoginVC: UIViewController {
     }
     
     @IBAction func loginPressed(_ sender: Any) {
-        
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let novoViewController = storyBoard.instantiateViewController(withIdentifier: "ViewViaCodigo")
+        self.present(novoViewController, animated: true, completion: nil)
+   
+        guard let username = user.text, let password = password.text else {return}
+        let parameters = ["user": username, "password": password]
+        networkingService.request(endpoint: "/login", paramters: parameters) { (result) in
+            print(result)
+
+        }
+        print(username)
+        print(password)
+   }
     }
     
    
-}
+
